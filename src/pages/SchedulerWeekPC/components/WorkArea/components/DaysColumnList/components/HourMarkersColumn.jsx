b3fb381f77be6c4f4400480f2686c291bd3413b8
nikel <r0075301net.r@yandex.ru>
2021-06-11 16:17:42 +0300

    Kristinas work. HourMarkersColumn PR

--- src/pages/SchedulerWeekPC/components/WorkArea/components/DaysColumnList/components/HourMarkersColumn.jsx
+++ src/pages/SchedulerWeekPC/components/WorkArea/components/DaysColumnList/components/HourMarkersColumn.jsx
@@ -1,11 +1,30 @@
 import styled from "styled-components";
+import React from "react";
 
-const HourMarkersColumn = styled.div`
+const HourMarkersBackground = styled.div`
 	grid-row-start: 2;
 	position: relative;
-	background: #efefef;
+	/* background: #efefef; */
 	grid-column-start: 1;
+	border:1px solid red;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 `;
-HourMarkersColumn.displayName = "HourMarkersColumn";
+HourMarkersBackground.displayName = "HourMarkersBackground";
 
-export { HourMarkersColumn };
+export class HourMarkersColumn extends React.Component {
+	hours = [ "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00" ];
+	render() {
+
+		return (
+			<HourMarkersBackground>
+				{ this.hours.map( itemHours =>
+					<span>
+						{ itemHours }
+					</span>
+				) }
+			</HourMarkersBackground>
+		);
+	}
+}
