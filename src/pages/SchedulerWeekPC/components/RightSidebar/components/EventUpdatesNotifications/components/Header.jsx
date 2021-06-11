1f2b1d452226a113c6fd9ba913ee4c13f9771039
nikel <r0075301net.r@yandex.ru>
2021-06-12 01:56:23 +0300

    Fixed formating

--- src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/Header.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/Header.jsx
@@ -3,16 +3,15 @@ import React from "react";
 import styled from "styled-components";
 
 const HeaderNotificationCard = styled.div`
-  position: relative;
-  font-size: 15px;
-  line-height: 16px;
-  text-align: center;
-  border-bottom: 2px solid #000000;
-  font-family: 'IBM Plex Sans', sans-serif;
+	position: relative;
+	font-size: 15px;
+	line-height: 16px;
+	text-align: center;
+	border-bottom: 2px solid #000000;
+	font-family: 'IBM Plex Sans', sans-serif;
 	font-size: 15px;
 	font-weight: 600;
 `;
-
 HeaderNotificationCard.displayName = "HeaderNotificationCard";
 
 export class Header extends React.Component {
