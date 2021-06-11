3bbf96ef95377e1decb49734ecb4f4c3f3ffb708
Dreter <vladgrabko@gmail.com>
2021-06-12 01:42:07 +0300

    Сделана верстка кнопки MarkAllAsReadButton

--- src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/MarkAllAsReadButton.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/MarkAllAsReadButton.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const MarkAllAsReadButtonText = styled.button`
+	position: relative;
+	width:100%;
+	padding: 5px;
+	font-family: 'IBM Plex Sans', sans-serif;
+	font-weight: 500;
+	font-size: 15px;
+	line-height: 16px;
+	text-align: center;
+`;
 
 export class MarkAllAsReadButton extends React.Component {
 	render() {
 		return (
-			<div>
-
-			</div>
+			<MarkAllAsReadButtonText>
+				Очистить Уведомление
+			</MarkAllAsReadButtonText>
 		);
 	}
 }
