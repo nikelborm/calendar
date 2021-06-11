47bb47af8504b265b4cdc29346ec81e5cfc5fe47
nikel <r0075301net.r@yandex.ru>
2021-06-12 01:42:44 +0300

    little fix

--- src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/NotificationContainer.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/NotificationContainer.jsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 export const NotificationContainer = styled.div`
 	position: relative;
 	top: 20px;
-	border: 1px solid #000000;
-	/* width: 350px; */
+	border: 2px solid #000000;
+	width: 300px;
 	border-radius: 2px;
+	height: 475px;
+	margin: 0 auto;
 `;
 NotificationContainer.displayName = "NotificationContainer";
--- src/pages/SchedulerWeekPC/components/RightSidebar/components/SidebarContent.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/SidebarContent.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const SidebarContent = styled.div`
-	background-color: grey;
 	padding: 20px;
 `;
 SidebarContent.displayName = "SidebarContent";
