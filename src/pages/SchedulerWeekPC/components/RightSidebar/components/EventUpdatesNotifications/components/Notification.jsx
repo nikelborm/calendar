2eea936977e1918921d221a209a46ac257d50ef1
KirillFazi <49456329+KirillFazi@users.noreply.github.com>
2021-06-12 00:38:41 +0300

    С делал основную чать: пропсы и их отображение, структуру.

--- src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/Notification.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/Notification.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import styled from "styled-components";
 
+
+const NotificationCard = styled.div`
+	position: relative;
+	margin-bottom: 10px;
+`;
+
+const Header = styled.div`
+	
+`;
+
+const Name = styled.div`
+	
+`;
+
+const EventDate = styled.div`
+	
+`;
+
+const Classroom = styled.div`
+	
+`;
+// dateBegin.getDay() + " "
+// + dateBegin.getMonth() + ", "
+// + new Intl.DateTimeFormat('ru-RU', options).format(dateBegin).toString() + ", "
+// + dateBegin.getHours() + ":" + dateBegin.getMinutes() + " - "
+// + dateEnd.getHours() + ":" + dateEnd.getMinutes()
 export class Notification extends React.Component {
 	render() {
-		return (
-			<div>
 
-			</div>
+		const dateBegin = new Date(this.props.eventDateBegin);
+		const dateEnd = new Date(this.props.eventDateEnd);
+		return (
+			<NotificationCard>
+				<Header>{this.props.notificationType}</Header>
+				<Name>{this.props.eventName}</Name>
+				<EventDate>{
+					dateBegin.toLocaleDateString('ru',{weekday:'long', day:'2-digit', month:'long'}) +
+					", " + dateBegin.getHours() + ":" + dateBegin.getMinutes() +
+					" - " + dateEnd.getHours() + ":" + dateEnd.getMinutes()
+				}
+				</EventDate>
+				<Classroom>{this.props.eventClassroom}</Classroom>
+			</NotificationCard>
 		);
 	}
 }
