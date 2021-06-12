7598ee688d2e3ab4ede90acecb0732b2d57e7ad8
nikelborm <kolya007.klass@gmail.com>
2021-06-13 01:27:11 +0300

    изменённая версия

--- src/pages/SchedulerWeekPC/components/DayColumnList/components/HourLines.jsx
+++ src/pages/SchedulerWeekPC/components/DayColumnList/components/HourLines.jsx
@@ -2,26 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 const Line = styled.div`
-
-justify-content:space-between;
-grid-area: 2 / 2 / -1 / -1;
+	grid-row-start: 1;
+	grid-column-end:2;
+	justify-content:space-between;
+	display:flex;
 `;
 
 export class HourLines extends React.Component {
 	 
 	render() {
-		const lines = [<hr/>, <hr/>, <hr/>,<hr/>, <hr/>, <hr/>,<hr/>, <hr/>, <hr/>];
-		// for (let idx = 0; idx < 15; idx++){
-		// 	lines.push(<div>{idx}</div>);
-		// }
+		let lines = [];
+		for (let i =1; i<=15; i++)
+		lines.push(<hr/>);
+
 		return (
-			<Line>
+			<Line>	
 				{lines}
-				{/* { this.hours.map( itemLines =>
-					<span key={ itemLines }>
-						{ itemLines }
-					</span>
-				) } */}
 			</Line>
 		);
 	}
