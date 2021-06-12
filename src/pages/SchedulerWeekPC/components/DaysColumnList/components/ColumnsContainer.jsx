f1cd67145e55a14675fd20f697fcbd416e1c2768
nikelborm <kolya007.klass@gmail.com>
2021-06-12 04:14:26 +0300

    grid-fix

--- src/pages/SchedulerWeekPC/components/DaysColumnList/components/ColumnsContainer.jsx
+++ src/pages/SchedulerWeekPC/components/DaysColumnList/components/ColumnsContainer.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ColumnsContainer = styled.div`
 	background: #efefef;
 	display: grid;
-	grid-area: 2 / 1;
+	grid-area: 2 / 1 / 3 / 2;
 	grid-template-columns: 50px repeat( 7, 1fr ); // 7 - amount of days to show
 	grid-template-rows: 40px 1fr;
 `;
--- src/pages/SchedulerWeekPC/components/RightSidebar/components/SidebarContent.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/SidebarContent.jsx
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const SidebarContent = styled.div`
 	padding: 20px;
+	grid-area: 2 / 2 / 3 / 3;
 `;
 SidebarContent.displayName = "SidebarContent";
