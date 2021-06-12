a4f043b1473e0bbaae4cfca5f3a6a11b5929c336
nikel <r0075301net.r@yandex.ru>
2021-06-12 16:28:53 +0300

    Made EventCard more responsive

--- src/pages/SchedulerWeekPC/components/DayColumn/components/EventCard.jsx
+++ src/pages/SchedulerWeekPC/components/DayColumn/components/EventCard.jsx
@@ -20,15 +20,24 @@ export class EventCard extends React.PureComponent {
         const ratioOfEventAndColumnHeights = ( eventMarginBottomInMinutes - eventMarginTopInMinutes ) / minutesInEventsColumn;
         const ratioOfEventTopMarginAndColumnHeight = eventMarginTopInMinutes / minutesInEventsColumn;
 
+        let showEventClassroom = true;
+        let showEventName = true;
+        let showEventPeriod = true;
 
         let justify = "space-around";
         let indent = 0;
         let indentTop = 0;
 
-        if (ratioOfEventAndColumnHeights * 100 > 20) {
+        if (ratioOfEventAndColumnHeights > 0.2) {
             justify = "flex-start";
             indent = 10;
             indentTop = 10;
+        }
+        if (ratioOfEventAndColumnHeights < 0.1) {
+            showEventPeriod = false;
+        }
+        if (ratioOfEventAndColumnHeights < 0.05) {
+            showEventClassroom = false;
         }
         console.log(ratioOfEventAndColumnHeights * 1000);
 
@@ -38,10 +47,10 @@ export class EventCard extends React.PureComponent {
                 height={ ratioOfEventAndColumnHeights }
                 justify={ justify }
             >
-                <CardLine indent={ indent } indentTop={ indentTop }>
+                <CardLine indent={ indent } indentTop={ indentTop } show={ showEventName }>
                     { this.props.name }
                 </CardLine>
-                <CardLine indent={ indent }>
+                <CardLine indent={ indent } show={ showEventPeriod }>
                     { pad( dateBegin.getHours() ) }
                     :
                     { pad( dateBegin.getMinutes() ) }
@@ -50,7 +59,7 @@ export class EventCard extends React.PureComponent {
                     :
                     { pad( dateEnd.getMinutes() ) }
                 </CardLine>
-                <CardLine indent={ indent }>
+                <CardLine indent={ indent } show={ showEventClassroom }>
                     { this.props.classroom }
                 </CardLine>
             </CardContent>
