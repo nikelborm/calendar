c2072437b4638e5622e967d5fbf65fdaadc69e01
Krima222 <manackaakristina@gmail.com>
2021-06-12 23:56:57 +0300

    там пыталась по разному сделать

--- src/pages/SchedulerWeekPC/components/DayColumnList/components/HourLines.jsx
+++ src/pages/SchedulerWeekPC/components/DayColumnList/components/HourLines.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const Line = styled.div`
+
+justify-content:space-between;
+grid-area: 2 / 2 / -1 / -1;
+`;
 
 export class HourLines extends React.Component {
+	 
 	render() {
+		const lines = [<hr/>, <hr/>, <hr/>,<hr/>, <hr/>, <hr/>,<hr/>, <hr/>, <hr/>];
+		// for (let idx = 0; idx < 15; idx++){
+		// 	lines.push(<div>{idx}</div>);
+		// }
 		return (
-			<div>
-
-			</div>
+			<Line>
+				{lines}
+				{/* { this.hours.map( itemLines =>
+					<span key={ itemLines }>
+						{ itemLines }
+					</span>
+				) } */}
+			</Line>
 		);
 	}
 }
