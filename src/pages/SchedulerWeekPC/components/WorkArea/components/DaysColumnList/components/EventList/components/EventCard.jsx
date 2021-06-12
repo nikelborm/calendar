429f70710cfd0e8a429cf75290e1f851a1d84a30
Krima222 <manackaakristina@gmail.com>
2021-06-12 13:34:11 +0300

    Адаптивная карточка, меняет стили в зависимости от размера

--- src/pages/SchedulerWeekPC/components/WorkArea/components/DaysColumnList/components/EventList/components/EventCard.jsx
+++ src/pages/SchedulerWeekPC/components/WorkArea/components/DaysColumnList/components/EventList/components/EventCard.jsx
@@ -4,13 +4,18 @@ import styled from "styled-components";
 const CardContent = styled.div`
     position: absolute;
     box-sizing: border-box;
-    padding: 15px 0 5px 20px;
+    padding: 5px 0 5px 20px;
     left:5px;
     right:5px;
     top: ${ props => props.top * 100 + "%" };
     background: #FF2800;
     border-radius: 15px;
-    height:${ props => props.height * 100 + "%" };
+    height:${ props => props.height * 100 + "%" };   
+    display:flex;
+    flex-direction:column;
+    margin: 0;
+    justify-content: ${ props => props.justify};
+    
 `;
 CardContent.displayName = "CardContent";
 
@@ -19,13 +24,15 @@ const CardLine = styled.p`
     font-weight: bold;
     font-family: Arial;
     margin: 0;
-    margin-bottom: 10px;
+    margin-bottom: ${ props => props.indent + "px"};  
+    margin-top: ${ props => props.indentTop + "px"}; 
 `;
 CardLine.displayName = "CardLine";
 
 const pad = v => ( "" + v ).padStart( 2, "0" );
 
 export class EventCard extends React.PureComponent {
+    
     render() {
         const hourOfColumnBottom = 21;
         const hourOfColumnTop = 8;
@@ -39,15 +46,32 @@ export class EventCard extends React.PureComponent {
 
         const ratioOfEventAndColumnHeights = ( eventMarginBottomInMinutes - eventMarginTopInMinutes ) / minutesInEventsColumn;
         const ratioOfEventTopMarginAndColumnHeight = eventMarginTopInMinutes / minutesInEventsColumn;
+
+
+        let justify = "space-around";
+        let indent = 0;
+        let indentTop = 0;
+
+
+        if (ratioOfEventAndColumnHeights * 100 > 20) {
+            justify = "flex-start";
+            indent = 10;
+            indentTop = 10;
+        }
+        console.log(ratioOfEventAndColumnHeights * 1000);
+
+        
         return (
             <CardContent
                 top={ ratioOfEventTopMarginAndColumnHeight }
                 height={ ratioOfEventAndColumnHeights }
+                justify={ justify }
+                
             >
-                <CardLine>
+                <CardLine indent={ indent } indentTop={ indentTop }>
                     { this.props.name }
                 </CardLine>
-                <CardLine>
+                <CardLine indent={ indent }>
                     { pad( dateBegin.getHours() ) }
                     :
                     { pad( dateBegin.getMinutes() ) }
@@ -56,7 +80,7 @@ export class EventCard extends React.PureComponent {
                     :
                     { pad( dateEnd.getMinutes() ) }
                 </CardLine>
-                <CardLine>
+                <CardLine indent={ indent }>
                     { this.props.classroom }
                 </CardLine>
             </CardContent>
