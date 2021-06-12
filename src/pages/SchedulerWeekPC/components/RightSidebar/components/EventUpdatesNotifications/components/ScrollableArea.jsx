842cca5ce6e49b9dce26e7a0167a43ce367702f9
nikelborm <r0075301net.r@yandex.ru>
2021-06-12 19:49:53 +0300

    little fix

--- src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/ScrollableArea.jsx
+++ src/pages/SchedulerWeekPC/components/RightSidebar/components/EventUpdatesNotifications/components/ScrollableArea.jsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const ScrollableArea = styled.div`
 	overflow-y: auto;
-	height: 460px;
+	height: 370px;
 `;
 ScrollableArea.displayName = "ScrollableArea";
