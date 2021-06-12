e580ca27eaefb8a0c8ff98702484ec519f6ad6ab
Dreter <vladgrabko@gmail.com>
2021-06-13 00:58:43 +0300

    Поработал над позиционнированием полоски для разных экранов, не вышло, как это сделать?

--- src/pages/SchedulerWeekPC/components/DaysColumnList/components/CurrentMomentRedLine.jsx
+++ src/pages/SchedulerWeekPC/components/DaysColumnList/components/CurrentMomentRedLine.jsx
@@ -7,15 +7,14 @@ const Line = styled.div`
 	position: absolute;
 	top: ${props => (props.linePosition + "px")};
 	left: 45px;
-	
-	
+	z-index: 998;
 `;
 Line.dispalayName = "Line";
 
 const BlackLine = styled.div`
 	position: absolute;
 	border-top: 3px solid black;
-	width: 1238px;
+	width: 1520px;
 	top: 8px;
 	left: 5px;
 	
@@ -33,6 +32,8 @@ export class CurrentMomentRedLine extends React.Component {
 		super(props);
 		this.state = {
 			time: new Date(),
+			windowHeight: "",
+			windowWidth: ""
 		};
 	}
 
@@ -53,17 +54,21 @@ export class CurrentMomentRedLine extends React.Component {
 	render() {
 
 		let nowTime = new Date(this.state.time).getHours() * 60 + new Date(this.state.time).getMinutes();
+		let windowHeight = window.innerHeight;
+		let windowWidth = window.innerWidth;
+		const startPos = windowHeight;
+		const endPos = windowHeight;
 
 		let linePosition;
 		if (nowTime > 480 && nowTime < 1260){
-			let top = 95 + (nowTime - 480);
+			let top = 95  + (nowTime - 480);
 			linePosition = top * 1.03;
 		}
 		if (nowTime >= 1260){
-			linePosition = 900;
+			linePosition = endPos * 0.9;
 		}
 		if (nowTime <= 480){
-			linePosition = 95;
+			linePosition = startPos * 0.1;
 		}
 
 		console.log(nowTime);
@@ -75,7 +80,6 @@ export class CurrentMomentRedLine extends React.Component {
 					<circle cx="10" cy="10" r="6" stroke="black" stroke-width="1" fill="black" />
 					</svg>
 				</BlackDot>
-
 				<BlackLine></BlackLine>
 			</Line>
 		);
