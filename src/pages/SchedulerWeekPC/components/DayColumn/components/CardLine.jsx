7d43f31a0c3d1bfcdd7192edea9e37b639781a43
nikel <r0075301net.r@yandex.ru>
2021-06-12 17:47:54 +0300

    Bugfix

--- src/pages/SchedulerWeekPC/components/DayColumn/components/CardLine.jsx
+++ src/pages/SchedulerWeekPC/components/DayColumn/components/CardLine.jsx
@@ -4,8 +4,11 @@ export const CardLine = styled.p`
     display: ${ props => props.show ? "block" : "none" };
     font-weight: bold;
     font-family: Arial;
-    margin: 0;
-    ${ props => props.indent ?? `margin-bottom: ${ props.indent }px;` }
-    ${ props => props.indentTop ?? `margin-top: ${ props.indentTop }px;` }
+    margin:
+        ${ props => props.indentTop ? props.indentTop + "px" : "0" }
+        0
+        ${ props => props.indent ? props.indent + "px" : "0" }
+        0
+    ;
 `;
 CardLine.displayName = "CardLine";
