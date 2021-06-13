093b85b25991fbe57e6803e6115ee3cc8c253e74
nikelborm <kolya007.klass@gmail.com>
2021-06-13 14:40:35 +0300

    Полосочка готова

--- src/pages/SchedulerWeekPC/components/DaysColumnList/components/ColumnsContainer.jsx
+++ src/pages/SchedulerWeekPC/components/DaysColumnList/components/ColumnsContainer.jsx
@@ -6,5 +6,6 @@ export const ColumnsContainer = styled.div`
 	grid-area: 2 / 1 / 3 / 2;
 	grid-template-columns: 50px repeat( 7, 1fr ); // 7 - amount of days to show
 	grid-template-rows: 40px 1fr;
+	position: relative;
 `;
 ColumnsContainer.displayName = "ColumnsContainer";
