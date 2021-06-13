578f08bf62a59b82be4cd11aacc379011900d2e7
nikelborm <kolya007.klass@gmail.com>
2021-06-13 20:56:26 +0300

    made day pc events screen

--- src/components/WorkArea/components/ColumnsContainer.jsx
+++ src/components/WorkArea/components/ColumnsContainer.jsx
@@ -4,7 +4,6 @@ export const ColumnsContainer = styled.div`
 	background: #efefef;
 	display: grid;
 	grid-area: 2 / 1 / 3 / 2;
-	grid-template-columns: 50px repeat( ${ props => props.amountOfColumns }, 1fr ); // 7 - amount of days to show
-	grid-template-rows: 40px 1fr;
+	grid-template: ${ props => props.gridTemplateString };
 `;
 ColumnsContainer.displayName = "ColumnsContainer";
