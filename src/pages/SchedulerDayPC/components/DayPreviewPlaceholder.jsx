432ab89fae3969471270cdd5dee51ee56a00cf09
nikelborm <kolya007.klass@gmail.com>
2021-06-14 23:56:19 +0300

    Fixed commit

--- src/pages/SchedulerDayPC/components/DayPreviewPlaceholder.jsx
+++ src/pages/SchedulerDayPC/components/DayPreviewPlaceholder.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const DayPreview = styled.div`
     grid-area: 2 / 3 / -1 / -1;
-    background-color: #efefef;
+    background-color: #aquamarine;
     z-index: 5;
 `;
 DayPreview.displayName = "DayPreview";
