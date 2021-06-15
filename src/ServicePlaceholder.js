57fcb5d39c732ab8cb3cc60bb08c05cc0fa04cd4
Dreter <vladgrabko@gmail.com>
2021-06-15 16:56:45 +0300

    Коммит для работы на ноут #2

--- src/ServicePlaceholder.js
+++ src/ServicePlaceholder.js
@@ -208,4 +208,20 @@ export class Service {
             ]
         }
     }
+    static getEventsInfoView () {
+        return {
+            InfoView:[
+                {
+                    eventName: "Пр. Физика",
+                    eventDateBegin: "2021-06-15T12:00:00.000Z",
+                    eventDateEnd:   "2021-06-15T13:35:00.000Z",
+                    eventClassroom: "8 - 624",
+                    eventDescription: "Консультация перед экзаменом",
+                    eventTeacher: "Ершов Как-то там",
+                },
+            ]
+        }
+    }
+
 }
+
--- src/pages/SchedulerDayPC/components/EventInfoView.jsx
+++ src/pages/SchedulerDayPC/components/EventInfoView.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import { Service } from "../../../ServicePlaceholder";
 import {DayPreview} from "./DayPreviewPlaceholder";
 
 const PreviewHeader = styled.div`
@@ -9,15 +9,29 @@ const PreviewHeader = styled.div`
     font-size: 30px;
     text-align: center;
 `;
+const EventName = styled.div``;
+const EventTime = styled.div``;
+const EventClass = styled.div``;
+const EventDescription = styled.div``;
+const EventTeacher = styled.div``;
 
+const pad = v => ( "" + v ).padStart( 2, "0" );
 
 export class EventInfoView extends React.Component {
     render() {
+        const dateBegin = new Date(Service.getEventsInfoView().InfoView[0].eventDateBegin);
+        const dateEnd = new Date(Service.getEventsInfoView().InfoView[0].eventDateEnd);
+        console.log(dateEnd);
         return(
         <DayPreview>
             <PreviewHeader>
                 Предосмотр
             </PreviewHeader>
+            <EventName>{Service.getEventsInfoView().InfoView[0].eventName}</EventName>
+            <EventClass>{Service.getEventsInfoView().InfoView[0].eventClassroom}</EventClass>
+            <EventTime>{dateBegin.getHours()} : {dateBegin.getMinutes()} - {dateEnd.getHours()} : {dateEnd.getMinutes()} </EventTime>
+            <EventDescription>{Service.getEventsInfoView().InfoView[0].eventDescription}</EventDescription>
+            <EventTeacher>{Service.getEventsInfoView().InfoView[0].eventTeacher}</EventTeacher>
         </DayPreview>
         )
     }
